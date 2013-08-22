0183d96b06de3d77bfe076ee71039c5bcd965298
MateuszAdamowicz <adammatb@gmail.com>
2013-08-22 15:32:44 +0200

    na zywo

--- app/controllers/zaklady_controller.rb
+++ app/controllers/zaklady_controller.rb
@@ -17,7 +17,7 @@ class ZakladyController < ApplicationController
           data_2 = DateTime.new(2000+data[-1].to_i,data[-2].to_i,data[-3].to_i,godzina[0].to_i,godzina[1].to_i)
     			kursy = ["#{$5}".to_f, "#{$6}".to_f, "#{$7}".to_f]
         
-          @zaklad = Zaklad.new({:data => data_2, :gosc => gosc, :gospodarz => gospodarz, :k1 => kursy[0], :k2 => kursy[1], :k3 => kursy[2], :event_id => @event.id})
+          @zaklad = Zaklad.new({:data => data_2, :gosc => gosc.sub("na żywo",""), :gospodarz => gospodarz, :k1 => kursy[0], :k2 => kursy[1], :k3 => kursy[2], :event_id => @event.id})
           @zaklad.save
         elsif x =~ /(.+)\s-\s(.+)\s(\d+.\d+.\d+)\s(\d+:\d+)\s(\d+.\d+)\s(\d+.\d+)/
           gospodarz = "#{$1}"
